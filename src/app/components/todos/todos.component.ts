45c0b1cd01cd1d561e44e78ce4dc84d9c25604be
Maude <mualth@gmail.com>
2021-05-13 15:29:00 -0400

    set priority

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -20,10 +20,55 @@ export class TodosComponent implements OnInit {
       },
       {
         content: 'Meditate',
-        priority: 'Medium',
+        priority: 'High',
         completed: false
       },
     ]
+
+  }
+
+  toggleDone (id: number) {
+    this.todos.map((todo, i) => {
+      if (i == id) todo.completed = !todo.completed;
+      this.addFinishDate();
+      return todo;
+    })
+  }
+
+  addTodo () {
+    console.log("added, yay!");
+
+  }
+
+  deleteTodo (id: number) {
+    this.todos = this.todos.filter((value, i) => i != id);
+  }
+
+  setPriority (todo: any) {
+    // rewrite
+    let option;
+    switch(todo.priority){
+      case 'Low' :
+        option = 'Medium';
+        break;
+      case 'Medium' :
+        option = 'High';
+        break;
+      case 'High' :
+        option = 'Low';
+        break;
+      default:
+        option = 'Low';
+    }
+    return todo.priority = option
+
+  }
+
+  filterTodos () {
+
+  }
+
+  addFinishDate() {
 
   }
 
